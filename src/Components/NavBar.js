be01d47b35e50623cbebaad383f777cc5d259624
Sk. Imran Hussain <skimranpc@gmail.com>
2023-11-12 18:32:02 +0530

    added navbar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,20 @@
+import logo from "../Asset/logo.png"
+
+function NavBar() {
+    return(
+        <header className="header">
+            <div className="title">
+                <img src={logo} className="title-img"/>
+                <h1 className="heading">Tour With Imran</h1>
+            </div>
+            
+            <div className="nav-bar">
+                <p className="home"><i class="fa fa-fw fa-home"></i> Home</p>
+                <p className="wishlist"><i class="fa-solid fa-heart wish-icon"></i> Wishlist</p>
+            </div>
+        </header>
+        
+    )
+}
+
+export default NavBar
